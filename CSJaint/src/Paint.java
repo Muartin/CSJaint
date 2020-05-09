eaefa391c90ec4efc03ed523e41e1e76ffe1af6e
Yuding <32459243+Muartin@users.noreply.github.com>
2020-05-09 16:20:11 -0400

    Update Paint.java

--- CSJaint/src/Paint.java
+++ CSJaint/src/Paint.java
@@ -1,6 +1,60 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
-public class Paint {
+public class Paint extends JFrame implements MouseListener {
+	int mouseX = 0;
+	int mouseY = 0;
+	JFrame frame;
+
+	public Paint() {
+		setTitle("CSJaint");
+		addMouseListener(this);
+		setSize(900, 900);
+		setVisible(true);
+	}
+
 	public static void main(String[] args) {
-		
+		new Paint();
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		mouseX = e.getX();
+		mouseY = e.getY();
+		repaint();
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void paint(Graphics g) {
+		super.paint(g);
+		Graphics2D g2d = (Graphics2D) g;
+		g2d.fillOval(mouseX, mouseY, 30, 30);
 	}
 }
