9d01196cc7abd98c68c0e2a9332b783ed86a5c44
SJin-exe <65048277+SJin-exe@users.noreply.github.com>
2020-05-11 14:47:46 -0400

    Update Paint.java

--- CSJaint/src/Paint.java
+++ CSJaint/src/Paint.java
@@ -2,8 +2,15 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.event.MouseMotionListener;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 
 public class Paint extends JFrame implements MouseListener {
 	int mouseX = 0;
@@ -12,9 +19,23 @@ public class Paint extends JFrame implements MouseListener {
 
 	public Paint() {
 		setTitle("CSJaint");
-		addMouseListener(this);
-		setSize(900, 900);
-		setVisible(true);
+    		addMouseListener(this);
+    		addMouseMotionListener(this);
+
+   		setSize(1200, 900);
+    
+ 		JButton Select = new JButton("Select");
+    		Select.setBounds(110, 10, 100, 50);
+    		add(Select);
+    		JButton Eraser = new JButton("Eraser");
+    		Eraser.setBounds(10, 10, 100, 50);
+    		add(Eraser);
+    		JButton Clear = new JButton("Clear");
+   		Clear.setBounds(210, 10, 100, 50);
+    		add(Clear);
+
+    		setLayout(null);
+    		setVisible(true);
 	}
 
 	public static void main(String[] args) {
