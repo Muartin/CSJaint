869db3c75df512acb0da77dff5374cb774e9704a
SJin-exe <65048277+SJin-exe@users.noreply.github.com>
2020-05-12 14:35:41 -0400

    Update Paint.java

--- CSJaint/src/Paint.java
+++ CSJaint/src/Paint.java
@@ -2,80 +2,86 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.Color;
 import java.awt.event.MouseMotionListener;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JButton;
-import javax.swing.JLabel;
-import javax.swing.JTextField;
 
+public class Paint extends JFrame implements MouseListener, MouseMotionListener {
+  int mouseX = 0;
+  int mouseY = 0;
+  int prevX = 0;
+  int prevY = 0;
+  private JFrame frame;
+  private JPanel canvas;
+  private JPanel toolbar;
+  private JButton eraser;
+  private JButton draw;
+  private JButton clear;
 
-public class Paint extends JFrame implements MouseListener {
-	int mouseX = 0;
-	int mouseY = 0;
-	JFrame frame;
+  public Paint() {
 
-	public Paint() {
-		setTitle("CSJaint");
-    		addMouseListener(this);
-    		addMouseMotionListener(this);
+    setTitle("CSJaint");
+    addMouseListener(this);
+    addMouseMotionListener(this);
+    setSize(1200, 900);
+    setVisible(true);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+  }
 
-   		setSize(1200, 900);
+  public static void main(String[] args) {
+    new Paint();
+  }
+
+  @Override
+  public void mouseClicked(MouseEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void mousePressed(MouseEvent e) {
+    // TODO Auto-generated method stub
+    mouseX = e.getX();
+    mouseY = e.getY();
+    repaint();
+    prevX = mouseX;
+    prevY = mouseY;
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void mouseEntered(MouseEvent e) {
+    // TODO Auto-generated method stub
+  }
+
+  @Override
+  public void mouseExited(MouseEvent e) {
+    // TODO Auto-generated method stub
+
+  }
+
+  public void mouseMoved(MouseEvent e) {
+
+  }
+
+  public void mouseDragged(MouseEvent e) {
+    prevX = mouseX;
+    prevY = mouseY;
+    repaint();
+    mouseX = e.getX();
+    mouseY = e.getY();
+
+  }
+
+  public void paint(Graphics g) {
+    Graphics2D g2d = (Graphics2D) g;
+    g2d.drawLine(prevX, prevY, mouseX, mouseY);
     
- 		JButton Select = new JButton("Select");
-    		Select.setBounds(110, 10, 100, 50);
-    		add(Select);
-    		JButton Eraser = new JButton("Eraser");
-    		Eraser.setBounds(10, 10, 100, 50);
-    		add(Eraser);
-    		JButton Clear = new JButton("Clear");
-   		Clear.setBounds(210, 10, 100, 50);
-    		add(Clear);
-
-    		setLayout(null);
-    		setVisible(true);
-	}
-
-	public static void main(String[] args) {
-		new Paint();
-	}
-
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-	}
-
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		mouseX = e.getX();
-		mouseY = e.getY();
-		repaint();
-	}
-
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void paint(Graphics g) {
-		super.paint(g);
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.fillOval(mouseX, mouseY, 30, 30);
-	}
+  }
 }
