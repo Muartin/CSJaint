9d80af43c01a9a70fd1317ba9984ce6c93e256cf
SJin-exe <65048277+SJin-exe@users.noreply.github.com>
2020-05-18 13:55:05 -0400

    CSJaint Updated
    Implements A Menu - 
    BUGS: 
    A second menu bar appears when the application is initially opened, this needs to be fixed - possibly an initial if-conditional could fix this.
    Potential drawing lag. This could be a hardware issue as apposed to a software issue, so we can worry about this later.

--- CSJaint/src/Paint.java
+++ CSJaint/src/Paint.java
@@ -9,120 +9,140 @@ import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JButton;
+//menu imports
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
-public class Paint extends JFrame implements MouseListener, MouseMotionListener, KeyListener {
+public class Paint extends JPanel implements MouseListener, MouseMotionListener, KeyListener{
 
-	// Points to keep track of current and previous mouse locations respectively
-	Point curMouse;
-	Point prevMouse;
+  // TODO: actually implement this stuff in a useful way
+  
+  // Creates a BasicBrush object which stores brush data
+  Point prevMouse;
+  Point curMouse;
+  //Creates a BasicBrush object which stores brush data
+  private BasicBrush brush;
+  
+  JPanel panel = new JPanel();
 
-	// TODO: actually implement this stuff in a useful way
-	private JFrame frame;
-	private JPanel canvas;
-	private JPanel toolbar;
-	private JButton eraser;
-	private JButton draw;
-	private JButton clear;
+  public Paint() {
 
-	// Creates a BasicBrush object which stores brush data
-	private BasicBrush brush;
+    panel.setName("CSJaint");
+    
+    brush = new BasicBrush(20);
+    
+    addMouseListener(this);
+    addMouseMotionListener(this);
+    addKeyListener(this);
+    
+    curMouse = new Point();
+    prevMouse = new Point();
+    brush = new BasicBrush(20);
+    
+    setSize(900,900);
+    setVisible(true);
 
-	public Paint() {
+    setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+    setSize(1200, 900);
+    setVisible(true);
 
-		setTitle("CSJaint");
-		addMouseListener(this);
-		addMouseMotionListener(this);
-		addKeyListener(this);
+  }
+  
+  public static void main(String[] args) {
+    
+    JFrame frame = new JFrame();
+    frame.add(new Paint());
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setBounds(100,100,900,900);
+    
+    JMenuBar menuBar = new JMenuBar();
+    JMenu menu = new JMenu("Menu");
+    menuBar.add(menu);
+    JMenuItem item = new JMenuItem("Option One");
+    menu.add(item);
+    frame.setJMenuBar(menuBar);
+    
+    frame.setVisible(true);
+   
+  }
+  
+  public void paintComponent(Graphics g) {
+    //super.paintComponent(g);
+    int startCount = 0;
+    Graphics2D g2d = (Graphics2D) g;
+    // big dumb stupid so that the program doesn't draw a dot in the upper left hand corner when the program starts.
+    if (startCount == 0 && prevMouse.getX() == 0 && prevMouse.getY() == 0 && curMouse.getX() == 0 && curMouse.getY() == 0) 
+      startCount++;
+    else 
+      brush.draw(g2d, prevMouse, curMouse);
+  }
 
-		// set initial mouse positions all to zero
-		curMouse = new Point(0, 0);
-		prevMouse = new Point(0, 0);
-		brush = new BasicBrush(20);
+  public void mouseClicked(MouseEvent e) {
+    // TODO Auto-generated method stub
 
-		setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(1200, 900);
-		setVisible(true);
-	}
+  }
 
-	public static void main(String[] args) {
-		new Paint();
-	}
+  @Override
+  public void mousePressed(MouseEvent e) {
 
-	public void paint(Graphics g) {
-//		super.paint(g);
-		Graphics2D g2d = (Graphics2D) g;
-		brush.draw(g2d, prevMouse, curMouse);
-	}
+    // basic drawing method
+    curMouse.setLocation(e.getX(), e.getY());
+    prevMouse.setLocation(curMouse);
+    repaint();
+  }
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
+  @Override
+  public void mouseReleased(MouseEvent e) {
+    // TODO Auto-generated method stub
 
-	}
+  }
 
-	@Override
-	public void mousePressed(MouseEvent e) {
+  @Override
+  public void mouseEntered(MouseEvent e) {
+    // TODO Auto-generated method stub
+  }
 
-		// basic drawing method
-		curMouse.setLocation(e.getX(), e.getY());
-		prevMouse.setLocation(curMouse);
-		repaint();
-	}
+  @Override
+  public void mouseExited(MouseEvent e) {
+    // TODO Auto-generated method stub
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
+  }
 
-	}
+  public void mouseMoved(MouseEvent e) {
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
-	}
+  }
 
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
+  public void mouseDragged(MouseEvent e) {
+    prevMouse.setLocation(curMouse);
+    curMouse.setLocation(e.getX(), e.getY());
+    repaint();
 
-	}
+  }
 
-	public void mouseMoved(MouseEvent e) {
+  @Override
+  public void keyTyped(KeyEvent e) {
+    // TODO Auto-generated method stub
+    int key = e.getKeyChar();
+    if (key == '[') {
+      brush.setSize(brush.getSize() - 1);
+    }
+    if (key == ']') {
+      brush.setSize(brush.getSize() + 1);
+    }
 
-	}
+  }
 
-	public void mouseDragged(MouseEvent e) {
-		prevMouse.setLocation(curMouse);
-		curMouse.setLocation(e.getX(), e.getY());
-		repaint();
+  @Override
+  public void keyPressed(KeyEvent e) {
+    // TODO Auto-generated method stub
+    if (e.isControlDown() && e.getKeyCode() == KeyEvent.VK_Z) {
+      System.out.println("undo");
+    }
+  }
 
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		int key = e.getKeyChar();
-		if (key == '[') {
-			brush.setSize(brush.getSize() - 1);
-		}
-		if (key == ']') {
-			brush.setSize(brush.getSize() + 1);
-		}
-		
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
-		if(e.isControlDown() && e.getKeyCode() == KeyEvent.VK_Z) {
-			System.out.println("undo");
-		}
-	}
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+  @Override
+  public void keyReleased(KeyEvent e) {
+    // TODO Auto-generated method stub
+  }
 }
